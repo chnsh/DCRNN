fc9de93fb69a2bc87ed9484b83857bb0bb366b73
Chintan Shah <chnsh24@gmail.com>
2020-01-26 20:16:45 -0500

    Using pytorch dataset and dataloaders and reduced batch size

--- lib/utils.py
+++ lib/utils.py
@@ -7,47 +7,49 @@ import h5py
 import numpy as np
 import scipy.sparse as sp
 import tensorflow as tf
+import torch
 from scipy.sparse import linalg
+from torch.utils.data import TensorDataset, DataLoader
 
 
-class DataLoader(object):
-    def __init__(self, xs, ys, batch_size, pad_with_last_sample=True, shuffle=False):
-        """
-
-        :param xs:
-        :param ys:
-        :param batch_size:
-        :param pad_with_last_sample: pad with the last sample to make number of samples divisible to batch_size.
-        """
-        self.batch_size = batch_size
-        self.current_ind = 0
-        if pad_with_last_sample:
-            num_padding = (batch_size - (len(xs) % batch_size)) % batch_size
-            x_padding = np.repeat(xs[-1:], num_padding, axis=0)
-            y_padding = np.repeat(ys[-1:], num_padding, axis=0)
-            xs = np.concatenate([xs, x_padding], axis=0)
-            ys = np.concatenate([ys, y_padding], axis=0)
-        self.size = len(xs)
-        self.num_batch = int(self.size // self.batch_size)
-        if shuffle:
-            permutation = np.random.permutation(self.size)
-            xs, ys = xs[permutation], ys[permutation]
-        self.xs = xs
-        self.ys = ys
-
-    def get_iterator(self):
-        self.current_ind = 0
-
-        def _wrapper():
-            while self.current_ind < self.num_batch:
-                start_ind = self.batch_size * self.current_ind
-                end_ind = min(self.size, self.batch_size * (self.current_ind + 1))
-                x_i = self.xs[start_ind: end_ind, ...]
-                y_i = self.ys[start_ind: end_ind, ...]
-                yield (x_i, y_i)
-                self.current_ind += 1
-
-        return _wrapper()
+# class DataLoader(object):
+#     def __init__(self, xs, ys, batch_size, pad_with_last_sample=True, shuffle=False):
+#         """
+#
+#         :param xs:
+#         :param ys:
+#         :param batch_size:
+#         :param pad_with_last_sample: pad with the last sample to make number of samples divisible to batch_size.
+#         """
+#         self.batch_size = batch_size
+#         self.current_ind = 0
+#         if pad_with_last_sample:
+#             num_padding = (batch_size - (len(xs) % batch_size)) % batch_size
+#             x_padding = np.repeat(xs[-1:], num_padding, axis=0)
+#             y_padding = np.repeat(ys[-1:], num_padding, axis=0)
+#             xs = np.concatenate([xs, x_padding], axis=0)
+#             ys = np.concatenate([ys, y_padding], axis=0)
+#         self.size = len(xs)
+#         self.num_batch = int(self.size // self.batch_size)
+#         if shuffle:
+#             permutation = np.random.permutation(self.size)
+#             xs, ys = xs[permutation], ys[permutation]
+#         self.xs = xs
+#         self.ys = ys
+#
+#     def get_iterator(self):
+#         self.current_ind = 0
+#
+#         def _wrapper():
+#             while self.current_ind < self.num_batch:
+#                 start_ind = self.batch_size * self.current_ind
+#                 end_ind = min(self.size, self.batch_size * (self.current_ind + 1))
+#                 x_i = self.xs[start_ind: end_ind, ...]
+#                 y_i = self.ys[start_ind: end_ind, ...]
+#                 yield (x_i, y_i)
+#                 self.current_ind += 1
+#
+#         return _wrapper()
 
 
 class StandardScaler:
@@ -188,9 +190,16 @@ def load_dataset(dataset_dir, batch_size, test_batch_size=None, **kwargs):
     for category in ['train', 'val', 'test']:
         data['x_' + category] = np.swapaxes(data['x_' + category], 0, 1)
         data['y_' + category] = np.swapaxes(data['y_' + category], 0, 1)  # swap time and graph axis
-    data['train_loader'] = DataLoader(data['x_train'], data['y_train'], batch_size, shuffle=True)
-    data['val_loader'] = DataLoader(data['x_val'], data['y_val'], test_batch_size, shuffle=False)
-    data['test_loader'] = DataLoader(data['x_test'], data['y_test'], test_batch_size, shuffle=False)
+
+    train_dataset = TensorDataset(torch.FloatTensor(data['x_train']),
+                                  torch.FloatTensor(data['y_train']))
+    val_dataset = TensorDataset(torch.FloatTensor(data['x_val']), torch.FloatTensor(data['y_val']))
+    test_dataset = TensorDataset(torch.FloatTensor(data['x_test']),
+                                 torch.FloatTensor(data['y_test']))
+
+    data['train_loader'] = DataLoader(train_dataset, batch_size, shuffle=True)
+    data['val_loader'] = DataLoader(val_dataset, test_batch_size, shuffle=False)
+    data['test_loader'] = DataLoader(test_dataset, test_batch_size, shuffle=False)
     # data['scaler'] = scaler
     return data
 
